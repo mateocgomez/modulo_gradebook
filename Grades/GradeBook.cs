17b8a2623ec5832884f4b6b5ccf78f421a410615
mateocgomez <gomezmateo94@gmail.com>
2018-08-16 16:11:15 -0500

    BluePrint del programa aun sin insumo de infromacion

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    //Clase descrita llamada gradebook
+
+    class GradeBook
+    {
+
+        /*public gradebook es un constructor dentro de la clase para poder crear objetos y trabajar con ellos
+         mediante la palabra ctor es un code snippet en c# para crear el constructor, casi siempre tienen el mismo nombre de la clase como codigo limpio */
+
+
+
+        public GradeBook()
+        {
+            //grades es la variable que me asigna un score dentro de la clase
+            grades = new List<float>();
+
+
+        }
+
+        //En un metodo cuando vamos a devolver alguna instancia escribimos el metodo con la instancia que vamos a devolver 
+      
+            public GradeStatistics ComputeStatistcs()
+        {
+            return new GradeStatistics(); 
+        }
+
+        //AddGrade es una metodo de la clase GradeBook
+        //Cuando no queremos devolver nada en el metodo escribimos la palabra void
+
+        public void AddGrade(float grade)
+        {
+         
+            grades.Add(grade);
+        }
+
+
+        //A la variable grades le asignamos el la variable y el tipo de variable pero en el constructor construimos la nueva variable con su tipo de variable
+
+        List<float> grades;
+    }
+}
---
+++ Grades/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        //Metodo principal, el cual es el punto de entrada a la aplicación
+
+        static void Main(string[] args)
+        {
+            //Definimos variables de la clase GradeBook el cual se llamara book 
+            //Las clases son el todo para construir moldes como variables que van a ser usadas en el desarrollo del programa
+
+            GradeBook book = new GradeBook();
+
+            /* -A traves de esta variable podemos invocar metodos 
+               -Mediante este metodo podemos pasar parametros o argumentos anteriormente definidos en el metodo como un numero flotante 
+               -Mediante la f podemos pasar variables de doble precision y convertilos a flotantes*/
+
+            book.AddGrade(91);
+            book.AddGrade(85.9f);
+
+
+            /*Creamos una nueva clase la cual se va a llamar GradeStatistics y contendra el metodo computestatistics el cual va a procesar la informacion 
+               de las notas y nos va a dar el resultado */
+
+            GradeStatistics stats = book.ComputeStatistcs();
+            
+          
+
+            GradeBook book2 = new GradeBook();
+            book2.AddGrade(75);
+
+        }
+    }
+}
